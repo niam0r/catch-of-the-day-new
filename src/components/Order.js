d7717d0ef35a1b75e875d06cf98e786545785ff8
Romain niam0r <niamor@mm.st>
2018-03-15 18:32:27 +0000

    this.props.tagline

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Order extends React.Component {
   render() {
     return (
-      <div className="inventory">
+      <div className="order">
         Order
       </div>
     )
