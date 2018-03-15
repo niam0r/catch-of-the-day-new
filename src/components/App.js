41bea54f886be9581b1a1dbfe8d25d591c3906c8
Romain niam0r <niamor@mm.st>
2018-03-15 18:26:12 +0000

    created Order and Inventory

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
         <div className="menu">
           <Header />
         </div>
-        <Inventory />
         <Order />
+        <Inventory />
       </div>
     )
   }
