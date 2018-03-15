a6a84bdfd15a4abca710dbd6160d1bb00266edc7
Romain niam0r <niamor@mm.st>
2018-03-15 16:50:51 +0000

    created and finished Header

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <div>StorePicker</div>
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store -></button>
+      </form>
     )
   }
 }
