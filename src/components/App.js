31b7301700bc2b2cdbef89fabd1236b2c8b326e9
Romain niam0r <niamor@mm.st>
2018-03-16 15:55:14 +0000

    implemented sampleFishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
-
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
   addFish = fish => {
     // take a copy of the existing state
     const fishes = {...this.state.fishes};
@@ -18,6 +19,10 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -25,7 +30,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
