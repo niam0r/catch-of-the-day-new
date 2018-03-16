c05faf1a036cc813587343bb33a3708da6c77590
Romain niam0r <niamor@mm.st>
2018-03-16 21:33:16 +0000

    implemented addToOrder

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,15 +4,19 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
+
     return (
       <li className="menu-fish">
-        <img src={this.props.details.image} alt={this.props.details.name}/>
+        <img src={image} alt={name}/>
         <h3 className="fish-name">
           {name}
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Order</button>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+          {isAvailable? 'Add To Order' : 'Sold Out'}
+        </button>
       </li>
     );
   }
