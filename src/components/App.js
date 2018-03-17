5a01eee0b1a052a5cb41a74bacdd45caa2b63203
Romain niam0r <niamor@mm.st>
2018-03-17 16:55:51 +0000

    created EditFishForm

--- src/components/App.js
+++ src/components/App.js
@@ -75,6 +75,7 @@ class App extends React.Component {
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
+          fish={this.state.fishes}
         />
       </div>
     );
