5687d1bea4c0dc55fae91e0c1073db224b60638a
Romain niam0r <niamor@mm.st>
2018-03-19 16:28:01 +0000

    authentification logic implemented and working

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -4,6 +4,7 @@ import firebase from "firebase";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
 import Login from "./Login";
+// import default export, import { named export }
 import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
@@ -14,8 +15,25 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func
   };
 
+  state = {
+    uid: null,
+    owner: null
+  };
+
   authHandler = async (authData) => {
-    console.log(authData);
+    // look up the current store in the firebase DB
+    const store = await base.fetch(this.props.storeId, { context: this });
+    // claim it if there's no owner
+    if (!store.owner) {
+      await base.post(`${this.props.storeId}/owner`, {
+        data: authData.user.uid
+      });
+    }
+    // set the state of the inventory component to reflect the current user
+    this.setState({
+      uid: authData.user.uid,
+      owner: store.owner || authData.user.uid
+    })
   };
 
   authenticate = provider => {
@@ -27,7 +45,17 @@ class Inventory extends React.Component {
   };
 
   render() {
-    return <Login authenticate={this.authenticate} />;
+    // check if user is logged in, if not then authenticate
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />;
+    }
+
+    // check if they are not the owner of the store
+    if (this.state.uid !== this.state.owner) {
+      return <div><p>Sorry you are not the owner</p></div>
+    }
+
+    // they must be the owner, render inventory
     return (
       <div className="inventory">
         <h2>Inventory</h2>
