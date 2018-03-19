a8bfd083ce3e1292446bcd7b349e26d04d67e28c
Romain niam0r <niamor@mm.st>
2018-03-19 12:12:19 +0000

    created Login.js and started implementing Github login

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  <nav className="login">
+    <h2>Inventory Login</h2>
+    <p>Sign in to manage your store's inventory.</p>
+    <button
+      className="github"
+      onClick={() => this.props.authenticate("Github")}
+    >
+      Log In With Github
+    </button>
+  </nav>
+};
+
+export default Login;
